9d5d69171a3664804039b31074d77a8c1dac7abf
xucs007 <xucs007@noreply.gitcode.com>
2024-12-10 14:13:04 +0800

    fix mysql datetime and date error and long parsing

--- source/ddbc/drivers/mysqlddbc.d
+++ source/ddbc/drivers/mysqlddbc.d
@@ -1085,7 +1085,9 @@ public:
             return 0;
         if (v.convertsTo!(long))
             return v.get!(long);
-        throw new SQLException("Cannot convert field " ~ to!string(columnIndex) ~ ": '" ~ v.toString() ~ "' to long");
+	else if (v.convertsTo!(string))
+            return v.get!(string).to!long;
+        throw new SQLException("Cannot convert field " ~ to!string(columnIndex) ~ ": '" ~ v.toString() ~ "' to long. type=" ~ v.type.to!string);
     }
     override ulong getUlong(int columnIndex) {
         checkClosed();
